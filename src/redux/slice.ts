5e51284ffb721027e0090395e774a13a3dda2e6e
Arunkumar S <arunkumars@myads.com>
2024-04-11 19:53:54 +0530

    initial commit

---
+++ src/redux/slice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { MovieListType } from "../assets/types";
+
+const initialState: MovieListType = {};
+
+const movieSlice = createSlice({
+  name: "movie",
+  initialState,
+  reducers: {
+    getMovieListPending(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    getMovieListSuccess(state, action: PayloadAction<MovieListType>) {
+      state.loading = false;
+      state.list = action.payload;
+    },
+    getMovieListFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { getMovieListPending, getMovieListSuccess, getMovieListFailure } =
+  movieSlice.actions;
+
+export default movieSlice.reducer;
